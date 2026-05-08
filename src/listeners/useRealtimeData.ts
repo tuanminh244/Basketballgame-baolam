d6cacb72c873cdf5ecf5df1acb435576c7513b15
tuanminh244 <tuanminh244@gmail.com>
2026-05-09 05:41:19 +0700

    Delete useRealtimeData.ts

--- src/listeners/useRealtimeData.ts
+++
@@ -1,4 +0,0 @@
-export { useSystemConfig } from './useSystemConfigListener';
-export { useStatsListener } from './useStatsListener';
-export { useSummaryListener, useChildTasksListener } from './useDailyTaskListener';
-export { useApprovalQueueListener } from './useApprovalQueueListener';
