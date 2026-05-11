3aff6b7081a9369a0819b021a49def1f6aeaf5bb
tuanminh244 <tuanminh244@gmail.com>
2026-05-11 15:20:01 +0700

    Create dbService.ts

---
+++ src/services/dbService.ts
@@ -0,0 +1,3 @@
+// Compatibility bridge layer. KHÔNG VIẾT LOGIC VÀO FILE NÀY. CHỈ RE-EXPORT.
+export { submitTaskAtomic } from './taskService';
+export { approveTaskAtomic } from './approvalService';
